060cc966c9af38a203f8cedddac368c703f4109e
Florian <florian.tellier02@gmail.com>
2021-08-13 22:17:50 +0200

    responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -284,7 +284,7 @@ const handleImagesReturn = image => {
 
     </div>
 
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00539c" fill-opacity="1" d="M0,288L80,250.7C160,213,320,139,480,138.7C640,139,800,213,960,234.7C1120,256,1280,224,1360,208L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="svg_avis"><path fill="#00539c" fill-opacity="1" d="M0,288L80,250.7C160,213,320,139,480,138.7C640,139,800,213,960,234.7C1120,256,1280,224,1360,208L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
       <section className="avis">
         <h1>Écoutez ce que nos fidèles clients disent sur nous </h1>
         <div className="slide-container">
